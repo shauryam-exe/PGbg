5648c38030c74429913a504a16b21b0b77c030a1
IamBansal <abansal11ae@gmail.com>
2022-03-19 23:20:57 +0530

    Upload Fragment Added

--- app/src/main/java/com/example/pg/HomePageActivity.kt
+++ app/src/main/java/com/example/pg/HomePageActivity.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import android.widget.FrameLayout
 import com.example.pg.fragments.ChatFragment
 import com.example.pg.fragments.ProfileFragment
+import com.example.pg.fragments.UploadFragment
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ProfileFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, UploadFragment()).commit()
 
     }
 }
