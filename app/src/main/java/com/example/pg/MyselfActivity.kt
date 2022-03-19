d9f6fdf8b58e995577b19bf7372bab6596b08a4c
Shauryam Saxena <saxena.shauryam@gmail.com>
2022-03-19 23:37:32 +0530

    commit 2

---
+++ app/src/main/java/com/example/pg/MyselfActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyselfActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myself)
+    }
+}
