b4a4c2504e13dbd0cdf422f364e77416d75e8cca
IamBansal <abansal11ae@gmail.com>
2022-03-19 23:01:15 +0530

    Profile Fragment Added

--- app/src/main/java/com/example/pg/HomePageActivity.kt
+++ app/src/main/java/com/example/pg/HomePageActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import com.example.pg.fragments.ChatFragment
+import com.example.pg.fragments.ProfileFragment
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ChatFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ProfileFragment()).commit()
 
     }
 }
