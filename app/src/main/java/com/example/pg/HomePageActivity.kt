e95506453f8d03b42d24cd414e1808db20bff1b4
IamBansal <abansal11ae@gmail.com>
2022-03-20 18:28:31 +0530

    Bottom Navigation Added

--- app/src/main/java/com/example/pg/HomePageActivity.kt
+++ app/src/main/java/com/example/pg/HomePageActivity.kt
@@ -1,19 +1,66 @@
 package com.example.pg
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
 import android.widget.FrameLayout
 import com.example.pg.fragments.ChatFragment
 import com.example.pg.fragments.HomeFragment
 import com.example.pg.fragments.ProfileFragment
 import com.example.pg.fragments.UploadFragment
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.MenuItem
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomePageActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var addButton : FloatingActionButton
+    private var selectorFragment: Fragment? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ChatFragment()).commit()
+        bottomNavigationView = findViewById(R.id.bottomnavigationbar)
+        addButton = findViewById(R.id.fab)
+
+        bottomNavigationView.background = null
+
+        bottomNavigationView.menu.getItem(2).isEnabled = true
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment())
+            .commit()
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.HomeNav -> {
+                    selectorFragment = HomeFragment()
+                }
+                R.id.ChatNav -> {
+                    selectorFragment = ChatFragment()
+                }
+                R.id.notificationNav -> {
+                    selectorFragment = ChatFragment()
+                }
+                R.id.profileNav -> {
+                    selectorFragment = ProfileFragment()
+                }
+            }
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, selectorFragment!!).commit()
+
+            return@setOnNavigationItemSelectedListener true
+        }
+
+        addButton.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, UploadFragment()).commit()
+        }
 
     }
 }
