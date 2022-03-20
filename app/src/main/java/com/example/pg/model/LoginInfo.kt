f1cf020c09ed18c9565b8abab97afb3934202444
IamBansal <abansal11ae@gmail.com>
2022-03-20 18:48:11 +0530

    UI Fixed

--- app/src/main/java/com/example/pg/model/LoginInfo.kt
+++ app/src/main/java/com/example/pg/model/LoginInfo.kt
@@ -3,7 +3,7 @@ package com.example.pg.model
 import android.util.Log
 
 object LoginInfo {
-    val users = listOf(Pair("yash","password"),Pair("ankit","ankit"))
+    val users = listOf(Pair("yash","password"),Pair("ankit","ankit"),Pair("1","1"))
 
     fun searchUser(name: String, password: String): Boolean {
         Log.d("check","User Working !!!")
